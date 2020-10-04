686f2ceb76d52d39b304f78e2835e8ae2114198a
berkaltug <berkaltug96@gmail.com>
2020-10-04 14:42:05 +0300

    antique service halfway done

--- src/main/java/com/scopic/antiqueauction/service/AntiqueService.java
+++ src/main/java/com/scopic/antiqueauction/service/AntiqueService.java
@@ -1,4 +1,15 @@
 package com.scopic.antiqueauction.service;
 
+import com.scopic.antiqueauction.domain.entity.Antique;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface AntiqueService {
+    Page<Antique> getAllAntiques(int pageNo, Sort.Direction direction);
+    Optional<Antique> getAntiqueById(Integer id);
+    Optional<Antique> updateAntique(Antique antique);
+    Optional<Antique> deleteAntiqueById(Integer id);
 }
--- src/main/java/com/scopic/antiqueauction/service/implementation/AntiqueServiceImpl.java
+++ src/main/java/com/scopic/antiqueauction/service/implementation/AntiqueServiceImpl.java
@@ -1,6 +1,45 @@
 package com.scopic.antiqueauction.service.implementation;
 
+import com.scopic.antiqueauction.domain.entity.Antique;
+import com.scopic.antiqueauction.repository.AntiqueRepository;
 import com.scopic.antiqueauction.service.AntiqueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class AntiqueServiceImpl implements AntiqueService {
+
+    private AntiqueRepository antiqueRepository;
+
+    @Autowired
+    public AntiqueServiceImpl(AntiqueRepository antiqueRepository) {
+        this.antiqueRepository = antiqueRepository;
+    }
+
+    @Override
+    public Page<Antique> getAllAntiques(int pageNo, Sort.Direction direction) {
+        Pageable page= PageRequest.of(pageNo,10,Sort.by(direction,"price"));
+        return antiqueRepository.findAll(page);
+    }
+
+    @Override
+    public Optional<Antique> getAntiqueById(Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Antique> updateAntique(Antique antique) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Antique> deleteAntiqueById(Integer id) {
+        return Optional.empty();
+    }
 }
